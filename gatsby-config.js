6ef0e6f4cb5c4b2ec6355df026d63233a2ca0816
takanokana <iricocco3@gmail.com>
2021-03-04 19:41:21 +0900

    初回

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
+const dotenv = require('dotenv')
+if (process.env.ENVIRONMENT !== 'production') {
+  dotenv.config()
+}
+
 module.exports = {
   siteMetadata: {
-    title: "My Gatsby Site",
+    title: 'My Gatsby Site',
   },
-  plugins: [],
-};
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.spaceID,
+        accessToken: process.env.accessToken,
+      },
+    },
+  ],
+}
